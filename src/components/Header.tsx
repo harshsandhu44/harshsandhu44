fd4667fed7d99f8c6c3b06b91fbc501ecc43c348
Harsh Sandhu <hsandhu@speareducation.com>
2025-06-11 23:28:23 +0530

    refactor: Simplify Header component by removing unused pathname logic and updating link styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,7 @@
-"use client";
-
 import { NAV_LINKS } from "@/lib/constants";
-import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 const Header = () => {
-  const pathname = usePathname();
   const navLinks = NAV_LINKS;
 
   return (
@@ -16,10 +11,7 @@ const Header = () => {
           <Link
             key={link.href}
             href={link.href}
-            className={cn(
-              "text-sm font-medium",
-              pathname === link.href && "underline underline-offset-4"
-            )}
+            className="text-sm font-medium hover:underline underline-offset-8"
           >
             {link.label}
           </Link>
