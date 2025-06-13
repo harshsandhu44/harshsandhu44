43174f221463f83cd73ce1700ccec25b7a847cb5
Harsh Sandhu <hsandhu@speareducation.com>
2025-06-13 20:24:40 +0530

    feat: Enhance search functionality in Projects page to include technology filtering and streamline Input component usage

--- src/app/(public)/projects/page.tsx
+++ src/app/(public)/projects/page.tsx
@@ -4,8 +4,6 @@ import { Input } from '@/components/ui/input';
 import { PROJECTS } from '@/lib/constants';
 import { useState } from 'react';
 import ProjectCard from '../_components/project-card';
-import { Button } from '@/components/ui/button';
-import { SearchIcon } from 'lucide-react';
 
 const Projects = () => {
   const [search, setSearch] = useState('');
@@ -14,7 +12,10 @@ const Projects = () => {
   const filteredProjects = projects.filter(
     (project) =>
       project.label.toLowerCase().includes(search.toLowerCase()) ||
-      project.description.toLowerCase().includes(search.toLowerCase())
+      project.description.toLowerCase().includes(search.toLowerCase()) ||
+      project.technologies.some((technology) =>
+        technology.toLowerCase().includes(search.toLowerCase())
+      )
   );
 
   return (
@@ -23,17 +24,12 @@ const Projects = () => {
         <h2 className="text-4xl font-bold">Projects</h2>
       </section>
 
-      <div className="flex items-center gap-4">
-        <Input
-          placeholder="Search projects"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          className="w-64"
-        />
-        <Button variant="outline" size="icon">
-          <SearchIcon className="size-4" />
-        </Button>
-      </div>
+      <Input
+        placeholder="Search projects"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="w-64"
+      />
 
       <section className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {filteredProjects.map((project, index) => (
