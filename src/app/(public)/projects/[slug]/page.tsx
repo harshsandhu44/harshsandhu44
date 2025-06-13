43ba3c5e3b274ff8f0384d483a3ce3c28db606fa
Harsh Sandhu <hsandhu@speareducation.com>
2025-06-13 22:44:12 +0530

    feat: Introduce dynamic project slug handling and add new project "MoodSync" with corresponding markdown documentation for enhanced project display and navigation

--- src/app/(public)/projects/[slug]/page.tsx
+++ src/app/(public)/projects/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import { PROJECTS } from '@/lib/constants';
+
 const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const { slug } = await params;
   const { default: Project } = await import(`@/markdown/${slug}.mdx`);
@@ -6,7 +8,7 @@ const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
 };
 
 export function generateStaticParams() {
-  return [{ slug: 'moodsync-app' }];
+  return PROJECTS.map((project) => ({ slug: project.slug }));
 }
 
 export const dynamicParams = false;
