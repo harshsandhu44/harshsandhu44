f01f4a511ca35563450bd20435b603a571c8591a
Harsh Sandhu <hsandhu@speareducation.com>
2025-06-13 23:52:30 +0530

    feat: Enhance project layout with back navigation button and update button styling for improved user experience

--- src/app/(public)/projects/[slug]/layout.tsx
+++ src/app/(public)/projects/[slug]/layout.tsx
@@ -1,8 +1,19 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { ArrowLeftIcon } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+
 const Layout = ({ children }: React.PropsWithChildren) => {
+  const router = useRouter();
+
   return (
-    <div className="container prose dark:prose-invert prose-zinc">
-      {children}
-    </div>
+    <main className="container space-y-4">
+      <Button onClick={() => router.back()} variant="secondary" size="icon">
+        <ArrowLeftIcon />
+      </Button>
+      <div className="prose dark:prose-invert prose-zinc">{children}</div>
+    </main>
   );
 };
 
