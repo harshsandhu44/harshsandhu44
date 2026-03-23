5f74cecb99ebf0229544580f90e409c792f9c73e
harshsandhu44 <hsandhu@speareducation.com>
2026-03-23 22:38:27 +0530

    fix: remove dangerouslyAllowSVG from next.config

--- apps/profile/next.config.ts
+++ apps/profile/next.config.ts
@@ -5,9 +5,6 @@ const nextConfig: NextConfig = {
     viewTransition: true,
   },
   images: {
-    dangerouslyAllowSVG: true,
-    contentDispositionType: "attachment",
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [
       {
         protocol: "https",
